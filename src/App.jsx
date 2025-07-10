498536dbe484023c7e27a0c3bcf30370917b584d
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-07-10 23:04:49 +0100

    How normal variable, state varible work and Virtual dom work

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
 import './App.css'
 function App() {
- const testFunction = ()=>{
-  return [10,20];
- }
- const result = testFunction();
- console.log(result);
-   return (
+  let [valueForTest, setvalueForTest] = useState(0);
+  const changeVariable = () => {
+    valueForTest = valueForTest + 1;
+    setvalueForTest(valueForTest);
+
+  }
+
+
+  return (
     <>
-     
+      <button onClick={changeVariable}>Click</button>
+      <h1>I am okay {valueForTest}</h1>
     </>
   )
 }
