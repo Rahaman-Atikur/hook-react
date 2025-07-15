2c0af6b68aedc134691f5e7a560deeb14caef3dd
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-07-15 14:48:23 +0100

    UseEffect done

--- src/components/child.jsx
+++ src/components/child.jsx
@@ -1,10 +1,14 @@
-import React, { useState } from 'react';
-const child = () => {
-    const [fromApi, setFromApi] = useState([]);
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
-        .then((res) => res.json())
-        .then((data) => setFromApi(data));
+import React, { useEffect, useState } from 'react';
+const Child = () => {
+    const [data, setFromApi] = useState([]);
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/todos/1')
+            .then((res) => res.json())
+            .then((data) => setFromApi(data));
+    },[])
+
     console.log(data);
+
     return (
         <div>
             <h1>I am Child</h1>
@@ -12,4 +16,4 @@ const child = () => {
     );
 };
 
-export default child;
+export default Child;
