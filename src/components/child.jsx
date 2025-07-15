3fe0200d970200b0eb2e7e2bb57ab1c0683e8857
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-07-15 19:36:48 +0100

    Not done

--- src/components/child.jsx
+++ src/components/child.jsx
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from 'react';
 const Child = () => {
     const [value, setValue] = useState(0);
+    const [anotherValue, setAnotherValue] = useState([]);
     const handleClick = () => {
         setValue(value + 1);
     }
     useEffect(() => {
         console.log(value);
+        console.log("Mount");
     }, [value])
     return (
         <div>
             <p>Increment:{value}</p>
             <button onClick={handleClick}>Click</button>
+            <button onClick={ }>Decrement Button</button>
         </div>
     );
 };
